0885bf45bfa682142e5c454d936d2b94cf73b2b8
tdedetd <tdedetd@gmail.com>
2024-03-15 01:09:01 +0300

    index.html hot reload

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
 
@@ -10,6 +11,10 @@ module.exports = merge(common, {
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
       "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
-    }
+    },
+    static: {
+      directory: path.join(__dirname, './'),
+      serveIndex: true,
+    },
   }
 });
